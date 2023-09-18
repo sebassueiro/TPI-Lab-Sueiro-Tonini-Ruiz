80784fd9f390a99835bf760cd52cb0175eb430e4
sebasueiro <s.sueiro@hotmail.com>
2023-09-18 15:20:02 -0300

    loginRutiado

--- src/App.js
+++ src/App.js
@@ -1,42 +1,32 @@
-import React, { useState } from "react";
-import Shop from "./components/shop/Shop";
-import { Card, Col } from "react-bootstrap";
-import ShopFilter from "./components/shopFilter/ShopFilter";
-const products = [
-  { type: "Remera", price: 8000, color: "Azul", size: "M", amount: 3 },
-  { type: "Remera", price: 8000, color: "Rojo", size: "L", amount: 3 },
-  { type: "Remera", price: 8000, color: "Amarillo", size: "XL", amount: 3 },
-  { type: "Remera", price: 8000, color: "Naranja", size: "S", amount: 3 },
-  { type: "Remera", price: 8000, color: "Azul", size: "S", amount: 0 },
-  { type: "Remera", price: 8000, color: "Verde", size: "M", amount: 3 },
-];
+import {
+  Navigate,
+  RouterProvider,
+  createBrowserRouter,
+} from "react-router-dom";
+import MainPage from "./components/mainPage/MainPage";
+import PageNotFound from "./components/pageNotFound/PageNotFound";
+import Login from "./components/login/Login";
+
 const App = () => {
-  const [colorSelected, setcolorSelected] = useState(false);
-  const [productsFiltered, setProductsFiltered] = useState(products);
-
-  const selectHandler = (color) => {
-    setcolorSelected(color);
-    if (color === "Todos") {
-      setProductsFiltered(products);
-    } else {
-      const productsFiltered = products.filter(
-        (product) => product.color.toString() === color
-      );
-      setProductsFiltered(productsFiltered);
-    }
-  };
-
-  return (
-    <div>
-      <Col md={3}>
-        <Card>
-          <Card.Body className="p-4 m-4">PRODUCTOS</Card.Body>
-        </Card>
-      </Col>
-      <ShopFilter colorSelected={colorSelected} colorChange={selectHandler} />
-      <Shop products={productsFiltered} />
-    </div>
-  );
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Navigate to="/mainPage" replace />,
+    },
+    {
+      path: "/mainPage",
+      element: <MainPage />,
+    },
+    {
+      path: "/login",
+      element: <Login />,
+    },
+    {
+      path: "*",
+      element: <PageNotFound />,
+    },
+  ]);
+  return <RouterProvider router={router} />;
 };
 
 export default App;
